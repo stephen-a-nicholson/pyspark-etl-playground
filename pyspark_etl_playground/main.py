7357c6e9f218dd60b2ec5a2e36837b1df5a31370
Stephen Nicholson <stephennicholson14@gmail.com>
2024-12-11 16:27:12 +0000

    Fix formatting, add filter operation

--- pyspark_etl_playground/main.py
+++ pyspark_etl_playground/main.py
@@ -2,7 +2,7 @@
 
 import kagglehub
 from pyspark.sql import SparkSession
-from pyspark.sql import functions as f
+from pyspark.sql import functions as F
 
 
 def main():
@@ -14,7 +14,11 @@ def main():
     movie_reviews = spark.read.csv(path=path, header=True)
     print(movie_reviews.head())
 
-    movie_reviews.select("*").filter(f.col("audience_rating") > 77).show()
+    movie_reviews.select("*").filter(F.col("audience_rating") > 77).show()
+
+    movie_reviews.select("movie_title").filter(
+        (F.col("audience_rating") > 90) & (F.col("tomatometer_rating") > 90)
+    ).show()
 
 
 if __name__ == "__main__":
